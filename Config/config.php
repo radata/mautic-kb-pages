9174b08689a9c4085bbb3e27b5fbbdd6fb1bfc6d
Robert Golebiewski <rag.web@outlook.com>
2026-03-19 07:08:46 +0100

    Style tables in KB public views

--- Config/config.php
+++ Config/config.php
@@ -4,7 +4,7 @@ return [
     'name'        => 'Knowledge Base',
     'description' => 'Manage grouped support articles with knowledge base pages.',
     'author'      => 'Abdullah Kiser / Friendly Automate',
-    'version'     => '1.0.7',
+    'version'     => '1.0.8',
     'routes'      => [
         'main' => [
             'mautic_knowledgebase_index' => [
