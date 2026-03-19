c742e0aeeeab1b68da0bc090d0a7495e00b29be3
Robert Golebiewski <rag.web@outlook.com>
2026-03-19 07:07:26 +0100

    Resolve page tokens in KB content

--- Controller/PublicController.php
+++ Controller/PublicController.php
@@ -3,6 +3,7 @@
 namespace MauticPlugin\MauticKbPagesBundle\Controller;
 
 use Mautic\CoreBundle\Controller\CommonController;
+use Mautic\PageBundle\Model\PageModel;
 use MauticPlugin\MauticKbPagesBundle\Entity\KbPages;
 use MauticPlugin\MauticKbPagesBundle\Model\KbPagesModel;
 use Symfony\Component\HttpFoundation\Response;
@@ -36,8 +37,9 @@ class PublicController extends CommonController
             $this->renderView(
                 '@MauticKbPages/Public/group.html.twig',
                 $this->getPublicViewParameters([
-                    'group'    => $group,
-                    'articles' => $repo->findPublishedArticlesByGroup($group),
+                    'group'        => $group,
+                    'groupContent' => $this->renderPublicHtml($group->getContent()),
+                    'articles'     => $repo->findPublishedArticlesByGroup($group),
                 ])
             )
         );
@@ -56,8 +58,9 @@ class PublicController extends CommonController
             $this->renderView(
                 '@MauticKbPages/Public/article.html.twig',
                 $this->getPublicViewParameters([
-                    'article' => $article,
-                    'group'   => $article->getParent(),
+                    'article'        => $article,
+                    'articleContent' => $this->renderPublicHtml($article->getContent()),
+                    'group'          => $article->getParent(),
                 ])
             )
         );
@@ -73,8 +76,8 @@ class PublicController extends CommonController
         return array_merge(
             [
                 'kbSettings' => [
-                    'headerHtml'    => (string) $this->coreParametersHelper->get('kbpages_header_html', ''),
-                    'footerHtml'    => (string) $this->coreParametersHelper->get('kbpages_footer_html', ''),
+                    'headerHtml'    => $this->renderPublicHtml((string) $this->coreParametersHelper->get('kbpages_header_html', '')),
+                    'footerHtml'    => $this->renderPublicHtml((string) $this->coreParametersHelper->get('kbpages_footer_html', '')),
                     'customCss'     => (string) $this->coreParametersHelper->get('kbpages_custom_css', ''),
                     'containerWidth' => (int) $this->coreParametersHelper->get('kbpages_container_width', 960),
                 ],
@@ -83,10 +86,47 @@ class PublicController extends CommonController
         );
     }
 
+    private function renderPublicHtml(?string $html): string
+    {
+        if (empty($html)) {
+            return '';
+        }
+
+        $rendered = preg_replace_callback('/{pagelink=(\d+)}/', function (array $matches): string {
+            return $this->resolvePageLinkToken($matches[0], (int) $matches[1]);
+        }, $html);
+
+        return is_string($rendered) ? $rendered : $html;
+    }
+
+    private function resolvePageLinkToken(string $token, int $pageId): string
+    {
+        try {
+            $pageModel = $this->getPageModel();
+            $page      = $pageModel->getEntity($pageId);
+
+            if (null === $page) {
+                return $token;
+            }
+
+            return $pageModel->generateUrl($page, true);
+        } catch (\Throwable) {
+            return $token;
+        }
+    }
+
     private function getKnowledgebaseModel(): KbPagesModel
     {
         $model = $this->getModel('kbpages');
         \assert($model instanceof KbPagesModel);
+
+        return $model;
+    }
+
+    private function getPageModel(): PageModel
+    {
+        $model = $this->getModel('page');
+        \assert($model instanceof PageModel);
 
         return $model;
     }
